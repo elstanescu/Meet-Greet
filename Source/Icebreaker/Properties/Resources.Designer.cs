24e1120c3ae2946f3b0577ce9454c74a29e1641e
elstanescu <61014671+elstanescu@users.noreply.github.com>
2020-04-30 17:11:53 +0300

    for testing first

--- Source/Icebreaker/Properties/Resources.Designer.cs
+++ Source/Icebreaker/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Icebreaker.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -160,7 +160,7 @@ namespace Icebreaker.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to It&apos;s time for a match!.
+        ///   Looks up a localized string similar to It&apos;s time for a match! HAHA :).
         /// </summary>
         internal static string MatchUpCardTitleContent {
             get {
